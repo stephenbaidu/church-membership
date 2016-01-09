36e13a5bef93b10621568915881139dfabb844d0
Stephen Baidu <stephenbaidu@gmail.com>
2016-01-09 22:50:33 +0000

    Fixed livereload options for all javascripts

--- angular/Gruntfile.js
+++ angular/Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
         tasks: ['wiredep']
       },
       js: {
-        files: ['<%= yeoman.app %>/scripts/**/*.js', '<%= yeoman.app %>/config/**/*.js', '<%= yeoman.app %>/app/components/**/*.js'],
+        files: ['<%= yeoman.app %>/scripts/**/*.js', '<%= yeoman.app %>/config/**/*.js', '<%= yeoman.app %>/app/**/*.js'],
         tasks: ['newer:jshint:all'],
         options: {
           livereload: '<%= connect.options.livereload %>'
